3ea4caf654b97374e96d4020fb4611f8ddddbb22
Canada-Broccoli <chen0969@algonquinlive.com>
2026-06-25 11:55:41 -0400

    start working on opening git add .git add .

---
+++ src/components/opening/Opening_main.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="grid grid-cols-2 gap-2 p-5">
+        <RouterLink to="open-check-bar" class="bg-amber-200 p-3 rounded text-2xl text-center">Back Kitchen</RouterLink>
+        <RouterLink to="/open-check" class="bg-amber-200 p-3 rounded text-2xl text-center">Main Kitchen</RouterLink>
+        <RouterLink to="/open-check" class="bg-amber-200 p-3 rounded text-2xl text-center">White Room</RouterLink>
+        <RouterLink to="/open-check" class="bg-amber-200 p-3 rounded text-2xl text-center">Bar</RouterLink>
+        <RouterLink to="/open-check" class="bg-amber-200 p-3 rounded text-2xl col-span-2 text-center">Patio</RouterLink>
+    </div>
+</template>
